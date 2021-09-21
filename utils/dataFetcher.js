48a1ebe91a9f467b2af00d6fbd104935a005660f
hugo.delpia <hugo.delpia@canal-plus.com>
2021-09-21 19:54:44 +0200

    set api url

--- utils/dataFetcher.js
+++ utils/dataFetcher.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://192.168.1.24:1337"; //TODO
+export const BASE_URL = "https://jeremy-capeau-back-j354g.ondigitalocean.app";
 
 export const fetchDataFromAPI = async (
   apiEndpoint,
